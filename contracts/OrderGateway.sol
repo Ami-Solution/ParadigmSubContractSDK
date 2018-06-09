109938ffe6d2abcfdbe8c0bd6ef17d73c42c1779
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-06-09 01:46:06 -0500

    Adding very rough Gateway and subcontract interface.

---
+++ contracts/OrderGateway.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.24;
+
+//import "../installed_contracts/zeppelin/contracts/token/StandardToken.sol";
+import "./SubContract.sol";
+
+
+
+contract OrderGateway {
+
+    event DealAdded(address dealOwner, uint timestamp, address modelAddress, address desiredToken, address paymentToken);
+
+    function participate(address dealModel, address[] addresses, uint[] uints) returns (bool) {
+        return SubContract(dealModel).participate(addresses, uints);
+    }
+}
---
+++ contracts/SubContract.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.24;
+
+
+contract SubContract {
+  function participate(address[], uint[]) returns (bool);
+}
