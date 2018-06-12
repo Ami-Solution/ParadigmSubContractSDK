8b87045e0c2145bd5c8d6f3be516f8e38ac61ab7
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-06-12 12:02:04 -0500

    Adding generated test

---
+++ test/zero_ex_sub_contract.js
@@ -0,0 +1,7 @@
+contract('ZeroExSubContract', function(accounts) {
+  it("should assert true", function(done) {
+    var zero_ex_sub_contract = ZeroExSubContract.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
