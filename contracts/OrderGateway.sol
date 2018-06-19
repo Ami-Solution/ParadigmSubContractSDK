3c4c5542175648883642ea00fa1727213bf78fa6
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-06-19 16:09:38 -0500

    removing extra lines.

--- contracts/OrderGateway.sol
+++ contracts/OrderGateway.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.4.24;
 //import "../installed_contracts/zeppelin/contracts/token/StandardToken.sol";
 import "./SubContract.sol";
 
-
-
 contract OrderGateway {
 
     event DealAdded(address dealOwner, uint timestamp, address modelAddress, address desiredToken, address paymentToken);
