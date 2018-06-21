ae3898f1c4f644de873a6891529bccd4e9efe0d3
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-06-20 19:50:55 -0500

    removing migrated toBytes32 functionality and its dependencies

--- test/init.js
+++ test/init.js
@@ -1,14 +1,8 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
-const Web3 = require('web3');
 
 chai.use(chaiAsPromised);
 
-global.utils = {
-  toBytes32: (value) => {
-    return Web3.utils.toTwosComplement(Web3.utils.toHex(value));
-  }
-}
 before(() => {
   expect = chai.expect
   chai.should()
