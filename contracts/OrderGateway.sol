2f8177c2233c2d3f570f2e90d3ffd59df399bcfb
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-06-25 11:03:38 -0500

    Creating a new ParadigmBank to be owned directly by the Gateway

--- contracts/OrderGateway.sol
+++ contracts/OrderGateway.sol
@@ -1,10 +1,17 @@
 pragma solidity ^0.4.24;
 
 import "./SubContract.sol";
+import "./ParadigmBank.sol";
 
 contract OrderGateway {
 
+    ParadigmBank public paradigmBank;
+
     event DealAdded(address dealOwner, uint timestamp, address modelAddress, address desiredToken, address paymentToken);
+
+    constructor() public {
+        paradigmBank = new ParadigmBank();
+    }
 
     function participate(address subContract, bytes32[] data) public returns (bool) {
         return SubContract(subContract).participate(data);
