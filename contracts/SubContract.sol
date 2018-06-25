d099f3deac6bb46ce058904da092adbf8b401e0b
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-06-25 11:03:07 -0500

    Adding ParadigmBank to all SubContracts

--- contracts/SubContract.sol
+++ contracts/SubContract.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.4.24;
 
+import "./ParadigmBank.sol";
 
 contract SubContract {
   string public dataTypes;
+  ParadigmBank public paradigmBank;
+
   function participate(bytes32[]) public returns (bool);
 }
