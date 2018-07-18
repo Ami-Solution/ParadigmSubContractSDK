93b62c2d407cf5ce05aa91ab3687dce0db429491
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-07-18 09:54:50 -0500

    Removing SignatureVerification variable

--- contracts/SubContract.sol
+++ contracts/SubContract.sol
@@ -1,20 +1,18 @@
 pragma solidity ^0.4.24;
 
 import "./ParadigmBank.sol";
-import "./SignatureVerification.sol";
 
 contract SubContract {
-  string public dataTypes;
-  ParadigmBank public paradigmBank;
-  SignatureVerification public verifier;
+    string public dataTypes;
+    ParadigmBank public paradigmBank;
 
-  function participate(bytes32[] data) public returns (bool);
+    function participate(bytes32[] data) public returns (bool);
 
-  function getSigner(bytes32[] data) view public returns (address) {
-    return address(data[0]);
-  }
+    function getSigner(bytes32[] data) view public returns (address) {
+        return address(data[0]);
+    }
 
-  function getOrderHash(bytes32[] data) returns (bytes32) {
-    return keccak256(getSigner(data));
-  }
+    function getOrderHash(bytes32[] data) returns (bytes32) {
+        return keccak256(getSigner(data));
+    }
 }
