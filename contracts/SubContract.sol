f5810b77bc03431bfe8cc00838a35b091dcd9fdb
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-07-25 09:44:12 -0500

    Adding a shared ratio calculation

--- contracts/SubContract.sol
+++ contracts/SubContract.sol
@@ -1,10 +1,17 @@
 pragma solidity ^0.4.24;
 
 import "./ParadigmBank.sol";
+import "../installed_contracts/zeppelin/contracts/math/SafeMath.sol";
 
 contract SubContract {
+    using SafeMath for uint;
+
     string public dataTypes;
     ParadigmBank public paradigmBank;
 
     function participate(bytes32[] data) public returns (bool);
+
+    function ratioFor(uint value, uint numerator, uint denominator) internal pure returns (uint) {
+        return value.mul(numerator).div(denominator);
+    }
 }
