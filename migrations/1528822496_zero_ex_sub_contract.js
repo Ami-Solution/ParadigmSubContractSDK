16aabf2311e3b2f4ebfecd2e7ee8bd902a86e0b2
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-07-31 16:02:21 -0500

    Getting the network to determine the addresses for contract deployment.

--- migrations/1528822496_zero_ex_sub_contract.js
+++ migrations/1528822496_zero_ex_sub_contract.js
@@ -1,13 +1,32 @@
 const ZeroExSubContract = artifacts.require("./ZeroExSubContract.sol");
 const OrderGateway = artifacts.require("./OrderGateway.sol");
 const ZeroExSubContractConfig = require('../configuration/ZeroExSubContract');
+const ZeroEx = require('0x.js').ZeroEx;
 
 module.exports = function(deployer) {
   deployer.then(async () => {
+
+    let resolve;
+    let getNetwork = new Promise((r) => { resolve = r; });
+    let networkId = -1;
+
+    web3.version.getNetwork((err, netId) => {
+      networkId = parseInt(netId);
+      resolve('');
+    });
+
+    await Promise.all([getNetwork]);
+
+    const zeroEx = new ZeroEx(web3.currentProvider, { networkId });
+    const zeroExExchange = zeroEx.exchange.getContractAddress();
+    const zeroExProxy = zeroEx.proxy.getContractAddress();
+
+    console.log(zeroExExchange, zeroExProxy);
+
     await deployer.deploy(
       ZeroExSubContract,
-      '0x48BaCB9266a570d521063EF5dD96e61686DbE788',
-      '0x1dc4c1cefef38a777b15aa20260a54e584b16c48',
+      zeroExExchange,
+      zeroExProxy,
       await (await OrderGateway.deployed()).paradigmBank.call(),
       JSON.stringify(ZeroExSubContractConfig.makerDataTypes),
       JSON.stringify(ZeroExSubContractConfig.takerDataTypes)
