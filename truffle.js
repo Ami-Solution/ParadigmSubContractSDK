d42ab71030caa50758bc2ec9a3976c9c1dba5b71
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-08-06 10:03:47 -0500

    Updating staging config.

--- truffle.js
+++ truffle.js
@@ -1,20 +1,24 @@
-var WalletProvider = require("truffle-wallet-provider");
-
-var keystore = require('./keystore.json');
-var wallet = require('ethereumjs-wallet').fromV3(keystore, process.env.keystore_password);
-
-module.exports = {
+const config = {
   networks: {
     test: {
       network_id: 50,
       host: 'localhost',
       port: 8545
-    },
-    staging: {
+    }
+  }
+};
+
+if(process.env.keystore_password) {
+  const WalletProvider = require("truffle-wallet-provider");
+  const keystore = require('./keystore.json');
+  const wallet = require('ethereumjs-wallet').fromV3(keystore, process.env.keystore_password);
+
+  config.networks.staging = {
       network_id: 3,
       gas: 4600000,
       gasPrice: 40000000000,
       provider: new WalletProvider(wallet, "https://ropsten.infura.io")
-    }
   }
-};
+}
+
+module.exports = config;
